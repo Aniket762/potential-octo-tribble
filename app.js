7c365df871a43a5bec9ccd77977a7df20258b349
Aniket762 <aniketindian8@gmail.com>
2020-12-23 13:43:26 +0530

    routing done

--- app.js
+++ app.js
@@ -16,12 +16,88 @@ app.get('/scala',(req,res)=>{
     res.render('scala');
 });
 
+app.get('/angular',(req,res) =>{
+  res.render('materials/angular');
+});
+
+app.get('/android',(req,res) =>{
+  res.render('materials/android');
+});
+
+app.get('/bootstrap',(req,res) =>{
+  res.render('materials/bootstrap');
+});
+
+app.get('/c',(req,res) =>{
+  res.render('materials/c');
+});
+
+app.get('/csharp',(req,res) =>{
+  res.render('materials/c#');
+});
+
+app.get('/datastructure',(req,res) =>{
+  res.render('materials/datastructure');
+});
+
+app.get('/datastructure',(req,res) =>{
+  res.render('materials/datastructure');
+});
+
 app.get('/deeplearning',(req,res) =>{
   res.render('materials/deeplearning');
 });
 
-app.get('/angular',(req,res) =>{
-  res.render('materials/angular');
+app.get('/django',(req,res) =>{
+  res.render('materials/django');
+});
+
+app.get('/git',(req,res) =>{
+  res.render('materials/git');
+});
+
+app.get('/go',(req,res) =>{
+  res.render('materials/go');
+});
+
+app.get('/htmlcss',(req,res) =>{
+  res.render('materials/htmlcss');
+});
+
+app.get('/machinelearning',(req,res) =>{
+  res.render('materials/machinelearing');
+});
+
+app.get('/matlab',(req,res) =>{
+  res.render('materials/matlab');
+});
+
+app.get('/mongodb',(req,res) =>{
+  res.render('materials/mongodb');
+});
+
+app.get('/nodejs',(req,res) =>{
+  res.render('materials/nodejs');
+});
+
+app.get('/python',(req,res) =>{
+  res.render('materials/python');
+});
+
+app.get('/react',(req,res) =>{
+  res.render('materials/react');
+});
+
+app.get('/rlanguage',(req,res) =>{
+  res.render('materials/rlanguage');
+});
+
+app.get('/ruby',(req,res) =>{
+  res.render('materials/ruby');
+});
+
+app.get('/webaccessiblity',(req,res) =>{
+  res.render('materials/webaccessiblity');
 });
 
 app.listen(3000,function(){
